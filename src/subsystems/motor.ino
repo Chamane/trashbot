f7a12bac399a01e6a1525e9703f079228dbdada5
Madsen W. J. Servius <madsen@madsendynamics.com>
2022-06-20 19:35:11 -0400

    init

---
+++ src/subsystems/motor.ino
@@ -0,0 +1,20 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;
+const int rolePerMinute = 5;
+
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+
+void setup() {
+  myStepper.setSpeed(rolePerMinute);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("clockwise");
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+  Serial.println("counterclockwise");
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
+}
